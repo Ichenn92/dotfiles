ba42faabd6a39a0179865b4d595e7f14762b1862
IchennDev <tristan.horler@gmail.com>
2024-07-05 17:38:03 +0200

    alacrity theme + firestore plugin

--- config/nvim/lua/core/vim-options.lua
+++ config/nvim/lua/core/vim-options.lua
@@ -1,6 +1,16 @@
 vim.g.mapleader = " "
 vim.g.background = "light"
 
+-- theme
+local themes = { "onedark_vivid", "github_light_high_contrast" }
+local current_theme_index = 1
+_G.switch_theme = function()
+  current_theme_index = 3 - current_theme_index -- Toggle between 1 and 2
+  vim.cmd("colorscheme " .. themes[current_theme_index])
+end
+vim.api.nvim_set_keymap('n', '<c-s>t', ':lua switch_theme()<CR>', { noremap = true, silent = true })
+
+
 -- core
 vim.opt.swapfile = false
 vim.opt.backup = false
--- config/nvim/lua/plugins/colorschemes.lua
+++ config/nvim/lua/plugins/colorschemes.lua
@@ -1,11 +1,16 @@
 return {
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000 -- Ensure it loads first
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000 -- Ensure it loads first
-  }
+	{
+		"olimorris/onedarkpro.nvim",
+		priority = 1000, -- Ensure it loads first
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000, -- Ensure it loads first
+	},
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
+	},
 }
